f399958d74ea3bc7f0c3bf0b3f2cc40d54d36043
Søren Malling <soren@meteko.dk>
2017-05-16 14:00:31 +0200

    Update whocopiedme.php

--- whocopiedme.php
+++ whocopiedme.php
@@ -5,7 +5,7 @@
  * Description: This plugin adds the javascript required from https://whocopied.me 
  * Version: 1.0.0
  * Author: Who Copied Me
- * Author URI: https://whocopied.me
+ * Author URI: https://whocopied.me/page/about
  * License: GPL2
  */
 function whocopiedme_javascript() {
