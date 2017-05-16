dd292137069c9413fcbe8c18c8589b661ca95567
Søren Malling <soren@meteko.dk>
2017-05-16 13:39:44 +0200

    Create whocopiedme.php

---
+++ whocopiedme.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Who Copied Me
+ * Plugin URI: https://whocopied.me
+ * Description: This plugin adds the javascript required from https://whocopied.me 
+ * Version: 1.0.0
+ * Author: Who Copied Me
+ * Author URI: https://whocopied.me
+ * License: GPL2
+ */
